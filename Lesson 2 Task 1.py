a045a7e409d456ec55032eac50e9fa23c5660f07
Sev1l <sevil.mammadli.std@bhos.edu.az>
2026-08-18 17:39:50 +0400

    Update Lesson 2 Task 1.py

--- Lesson 2 Task 1.py
+++ Lesson 2 Task 1.py
@@ -1,39 +1,36 @@
 ## Access modifiers
 # Task 1 (Supergroup)
 
-
 class SuperHero:
-    def __init__(self,name,types):
+    def __init__(self, name, types):
         self.name = name
         self.types = types
 
 
 class SuperGroup:
-    def __init__(self,name,location):
+    def __init__(self, name, location):
         self._name = name
         self._location = location
         self._members = []
 
     def get_name(self):
         return self._name
+
     def get_location(self):
         return self._location
 
-    def add_member(self,hero: SuperHero):
-        self._members += [hero]
+    def add_member(self, hero: SuperHero):
+        self._members.append(hero)
 
     def print_group(self):
-        print (f'{self._name}, {self._location} \nMembers:')
+        print(f'{self._name}, {self._location} \nMembers:')
         for hero in self._members:
             print(f'{hero.name}, superpowers: {hero.types}')
-         
-
 
 
 superperson = SuperHero("SuperPerson", "Superspeed, superstrength")
 invisible = SuperHero("Invisible Inca", "Invisibility")
 revengers = SuperGroup("Revengers", "Emerald City")
-
 revengers.add_member(superperson)
 revengers.add_member(invisible)
 revengers.print_group()
