54dc6bb053968f09fa147bb164161bc600a00c01
Sev1l <sevil.mammadli.std@bhos.edu.az>
2026-08-18 17:41:53 +0400

    Update Lesson 3 Task 2.py

--- Lesson 3 Task 2.py
+++ Lesson 3 Task 2.py
@@ -11,13 +11,13 @@ class SimpleDate:
     def __str__(self):
         return f'{self.result}'
     def __eq__(self,another):
-        return self.result == another.result
+        return (self.year, self.month, self.day) == (another.year, another.month, another.day)
     def __ne__(self,another):
-        return self.result != another.result
+        return (self.year, self.month, self.day) != (another.year, another.month, another.day)
     def __lt__(self,another):
-        return self.result < another.result
+        return (self.year, self.month, self.day) < (another.year, another.month, another.day)
     def __gt__(self,another):
-        return self.result > another.result
+        return (self.year, self.month, self.day) > (another.year, another.month, another.day)
     def __add__(self,another):
         summ = self.day + another
         year = self.year + summ // 365
@@ -27,11 +27,9 @@ class SimpleDate:
         month = month % 12
         summ = summ % 30
         return SimpleDate(summ,month,year)
-
 d1 = SimpleDate(4, 10, 2020)
 d2 = SimpleDate(28, 12, 1985)
 d3 = SimpleDate(28, 12, 1985)
-
 print(d1)
 print(d2)
 print(d1 == d2)
@@ -39,15 +37,11 @@ print(d1 != d2)
 print(d1 == d3)
 print(d1 < d2)
 print(d1 > d2)
-
 print()
-
 d1 = SimpleDate(4, 10, 2020)
 d2 = SimpleDate(28, 12, 1985)
-
 d3 = d1 + 3
 d4 = d2 + 400
-
 print(d1)
 print(d2)
 print(d3)
