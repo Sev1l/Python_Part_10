99d3f6298014dc56671120b94406d85ec7af32d9
Sev1l <sevil.mammadli.std@bhos.edu.az>
2026-08-18 17:37:47 +0400

    Update Lesson 1 Task 1.py

--- Lesson 1 Task 1.py
+++ Lesson 1 Task 1.py
@@ -2,17 +2,24 @@
 # Task 1 (Laptop computer)
 
 class Computer:
-    def __init__(self,model,speed):
+    def __init__(self, model, speed):
         self._model = model
         self._speed = speed
 
+    def get_model(self):
+        return self._model
+
+    def get_speed(self):
+        return self._speed
+
 
 class LaptopComputer(Computer):
-    def __init__(self,model,speed,weight):
-        super().__init__(model,speed)
+    def __init__(self, model, speed, weight):
+        super().__init__(model, speed)
         self.weight = weight
+
     def __str__(self):
-        return f'{self._model}, {self._speed} MHz, {self.weight} kg'
+        return f'{self.get_model()}, {self.get_speed()} MHz, {self.weight} kg'
 
 
 laptop = LaptopComputer("NoteBook Pro15", 1500, 2)
