82b06b1020a5bd5616c73b2d886c8a98ced7d153
Sev1l <sevil.mammadli.std@bhos.edu.az>
2026-08-18 17:38:49 +0400

    Update Lesson 1 Task 3.py

--- Lesson 1 Task 3.py
+++ Lesson 1 Task 3.py
@@ -4,23 +4,24 @@
 
 
 class Rectangle:
-    def __init__(self,first,second):
+    def __init__(self, first, second):
         self.first = first
         self.second = second
+
     def area(self):
         return self.first * self.second
+
     def __str__(self):
         return f'rectangle {self.first}x{self.second}'
 
 
 class Square(Rectangle):
-    def __init__(self,first):
-        super().__init__(first,first)
+    def __init__(self, first):
+        super().__init__(first, first)
+
     def __str__(self):
         return f'square {self.first}x{self.second}'
-    def area(self):
-        return super().area()
-    
+
 
 rectangle = Rectangle(2, 3)
 print(rectangle)
